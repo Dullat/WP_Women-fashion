652a7392078e54d849fd0b74b39b0c90bd47064b
dullat <jahandullat8@gmail.com.noreply.github.com>
2023-11-25 14:44:52 +0530

    ADDED: side bar effects

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ console.log("hahah");
 
 const menuToggle = document.querySelector('.menu-icon');
 const nav = document.querySelector('.primary-nav-container');
+const menuBtn = document.querySelector('.menu-icon');
 
 menuToggle.addEventListener('click', () => {
     let visiblity = nav.getAttribute('visiblity');
     console.log(visiblity);
     if( visiblity === "false"){
         nav.setAttribute('visiblity', 'true');
+        menuBtn.setAttribute('opened','true');
     }else if(visiblity === "true") {
-        nav.setAttribute('visiblity', 'false')
+        nav.setAttribute('visiblity', 'false');
+        menuBtn.setAttribute('opened','false');
     }
     console.log("clicked");
 })
